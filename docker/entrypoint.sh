67bd8c62ae962a7f9434451c2a9cfeb896f903c7
khaledmustafa91 <khaled.mustafa@cis.asu.edu.eg>
2022-11-03 23:08:06 +0200

    Remove unused files

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 /wait
 bundle exec rails db:exists && rails db:migrate || rails db:create db:migrate
-# rails db:exists && rails db:migrate || rails db:setup
-
-# rm /app/tmp/pids/server.pid
 
 puma -C config/puma.rb
