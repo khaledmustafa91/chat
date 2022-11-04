55b3b089acc22a972d5eb0d52a01e13fcc5a59d2
khaledmustafa91 <khaled.mustafa@cis.asu.edu.eg>
2022-11-04 14:47:43 +0200

    Rubocop

--- app/jobs/update_application_chat_count_job.rb
+++ app/jobs/update_application_chat_count_job.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class UpdateApplicationChatCountJob < ApplicationJob
   queue_as :default
 
--- config/initializers/sidekiq_cron.rb
+++ config/initializers/sidekiq_cron.rb
@@ -4,12 +4,12 @@ jobs = [
   {
     'name' => 'update_chat_message_count',
     'class' => 'UpdateChatMessageCountJob',
-    'cron' => 'every 1 hour',
+    'cron' => 'every 1 hour'
   },
   {
     'name' => 'update_application_chat_count',
     'class' => 'UpdateApplicationChatCountJob',
-    'cron' => 'every 1 hour',
-  },
+    'cron' => 'every 1 hour'
+  }
 ]
 Sidekiq::Cron::Job.load_from_array(jobs)
