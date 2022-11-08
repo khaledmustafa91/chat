b261c4a00a8cd3f32b15e37ce5eb7d3cdff80bc0
khaledmustafa91 <khaled.mustafa@cis.asu.edu.eg>
2022-11-08 23:40:42 +0200

    Add tests for messages

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Message, type: :model do
       expect(message).to be_valid
     end
   end
-  
+
   context 'with invalid attributes' do
     it 'returns success' do
       expect(invalid_message).to be_invalid
