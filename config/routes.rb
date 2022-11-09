b460cf1de11b5b6ca60a430123b89b257fcb4272
khaledmustafa91 <khaled.mustafa@cis.asu.edu.eg>
2022-11-10 00:04:13 +0200

    Remove unused routes for applications

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :applications, param: :token do
+  resources :applications, param: :token, except: [:destroy] do
     resources :chats, param: :number, controller: 'applications/chats' do
       resources :messages, param: :number, controller: 'applications/chats/messages' do
         collection do
